b387d0b5dd6f0dc20cd1ded27f5f53e490f39dd6
ashoka <ashokayewale2001@gmail.com>
2023-07-14 15:48:17 +0530

    add file

---
+++ firmware/src/build/V3_MCF1024/production/_ext/1375476428/device_stat.o.d
@@ -0,0 +1,12 @@
+build/V3_MCF1024/production/_ext/1375476428/device_stat.o: \
+ ../src/middleware/device_stat.c ../src/drivers/driver_UART.h \
+ ../src/config/V3_MCF1024/device.h \
+ ../src/config/V3_MCF1024/toolchain_specifics.h ../src/DEVSNfile.h
+
+../src/drivers/driver_UART.h:
+
+../src/config/V3_MCF1024/device.h:
+
+../src/config/V3_MCF1024/toolchain_specifics.h:
+
+../src/DEVSNfile.h:
